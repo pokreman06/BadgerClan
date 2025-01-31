8f7895c0b6a37a6c91f76f0a803fe56e005b412e
pokre <pokreman06@gmail.com>
2025-01-31 13:38:59 -0700

    finished logic didnt test

--- Client22/IFightStyle.cs
+++ Client22/IFightStyle.cs
@@ -1,4 +1,5 @@
 ﻿using BadgerClan.Logic;
+using System.Collections.Generic;
 namespace Client22;
 public class Mode
 {
@@ -48,7 +49,8 @@ public class Attack : IFightStyle
         var result = new Dictionary<UnitDto, Coordinate>();
         if (lastKnown != null)
         {
-            var ownUnits = lastKnown.Units.Where(p => p.Team == lastKnown.YourTeamId&&p.Type=="Knight").OrderBy<UnitDto, int>(p => p.Id).ToList();
+            var ownUnits = lastKnown.Units.Where(p => p.Team == lastKnown.YourTeamId && p.Type == "Knight").OrderBy<UnitDto, int>(p => p.Id).ToList();
+            var ownUnitsA = lastKnown.Units.Where(p => p.Team == lastKnown.YourTeamId && p.Type == "Archer").OrderBy<UnitDto, int>(p => p.Id).ToList();
             var baseLocation = ownUnits[0].Location;
             int left = 0;
             int right = 1;
@@ -69,16 +71,22 @@ public class Attack : IFightStyle
                 3 => baseLocation.MoveSouthEast,
                 _ => (p) => new(0, 0)
             };
-            foreach (var unit in ownUnits) 
+            foreach (var unit in ownUnits)
             {
                 result.Add(unit, isRight switch
                 {
-                    true=>rightFunc.Invoke(right*2),
-                    false => leftFunc.Invoke(left*2),
+                    true => rightFunc.Invoke(right * 2),
+                    false => leftFunc.Invoke(left * 2),
                 });
                 if (isRight) right++;
-                if(!isRight) left++;
+                if (!isRight) left++;
                 isRight = !isRight;
+
+            }
+            foreach (var unit in ownUnits)
+            {
+                result.Add(unit, baseLocation.Toward(baseLocation.MoveNorthEast(1).MoveNorthWest(1)));
+
 
             }
 
@@ -92,9 +100,10 @@ public class Attack : IFightStyle
     {
         lastKnown = request;
         var results = new List<Move>();
-        var otherUnits = request.Units.Where(p => p.Team != request.YourTeamId).ToList();
 
         var ownUnits = request.Units.Where(p => p.Team == request.YourTeamId).ToList();
+        var otherUnits = request.Units.Where(p => p.Team != request.YourTeamId).OrderBy<UnitDto, int>(p => p.Location.Distance(ownUnits[0].Location)).ToList();
+        //finding direction
         var col = ownUnits[0].Location.Col - otherUnits[0].Location.Col;
         var row = otherUnits[0].Location.Row - otherUnits[0].Location.Row;
         var direction = 0;
@@ -108,22 +117,78 @@ public class Attack : IFightStyle
         }
         else
         {
-            if(row >= 0)
+            if (row >= 0)
                 direction = 1;
             else
                 direction = 3;
         }
-        Dictionary<UnitDto, Coordinate> locations=TargetSpots(direction);
+        //setup
+        Dictionary<UnitDto, Coordinate> locations = TargetSpots(direction);
+        Dictionary<UnitDto, int> healths = new Dictionary<UnitDto, int>();
+        Dictionary<UnitDto, Coordinate> tempLocation = new Dictionary<UnitDto, Coordinate>();
+        int Medpacs = request.Medpacs;
+        foreach (UnitDto unit in request.Units)
+        {
+            healths.Add(unit, unit.Health);
+            tempLocation.Add(unit, unit.Location);
+            moves[unit] = 0;
+        }
+        //calc moves
         foreach (UnitDto unit in ownUnits)
         {
+            //unit setup
             otherUnits = otherUnits.OrderBy<UnitDto, int>(p => p.Location.Distance(unit.Location)).ToList();
-            moves[unit] = 0;
-            if(unit.Health<4)
+            var inRange = otherUnits.Where(p => p.Location.Distance(unit.Location) < unit.AttackDistance).OrderBy<UnitDto, int>(p => p.Location.Distance(unit.Location)).ToList();
+            //heal
+            if (unit.Health < 4 && Medpacs != 0)
             {
                 results.Add(new(MoveType.Medpac, unit.Id, unit.Location));
-                
+                Medpacs--;
                 moves[unit]++;
             }
+            //enemy in range
+            var target = inRange[0];
+            while (moves[unit] != unit.MaxMoves && inRange.Count != 0)
+            {
+                if (healths[target] <= 0)
+                {
+                    otherUnits.Remove(target);
+                    inRange.Remove(target);
+                    if (inRange.Count != 0)
+                        target = inRange[0];
+                    else
+                        break;
+                }
+                results.Add(new(MoveType.Attack, unit.Id, target.Location));
+                moves[unit]++;
+                healths[target] -= unit.Attack;
+            }
+            //move into position
+            while (moves[unit] != unit.MaxMoves && tempLocation[unit] != locations[unit]) ;
+            {
+                var occupied = request.Units.Where(p => p.Location == tempLocation[unit].Toward(locations[unit]));
+                if (occupied.Count() == 0 || healths[occupied.First()] == 0)
+                {
+                    results.Add(new(MoveType.Walk, unit.Id, tempLocation[unit].Toward(locations[unit])));
+                    tempLocation[unit] = tempLocation[unit].Toward(locations[unit]);
+                    moves[unit]++;
+                }
+                else break;
+            }
+            //advance
+            target = otherUnits[0];
+            while (moves[unit] != unit.MaxMoves)
+            {
+                var occupied = request.Units.Where(p => p.Location == tempLocation[unit].Toward(target.Location));
+                if (occupied.Count() == 0 || healths[occupied.First()] == 0)
+                {
+                    results.Add(new(MoveType.Walk, unit.Id, tempLocation[unit].Toward(target.Location)));
+                    tempLocation[unit] = tempLocation[unit].Toward(locations[unit]);
+                    moves[unit]++;
+                }
+                else break;
+            }
+
         }
         return results;
     }
