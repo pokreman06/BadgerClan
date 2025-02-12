4ab4092ff7c1f6c158e1ee8263efa08eac4993a7
pokre <pokreman06@gmail.com>
2025-02-12 14:02:16 -0700

    hello

--- grpc/Program.cs
+++ grpc/Program.cs
@@ -7,11 +7,17 @@ using ProtoBuf.Grpc.Server;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(8080);
-    options.ListenAnyIP(8585, listenoptions => listenoptions.Protocols = HttpProtocols.Http2);
-}
-    );
+    options.ListenAnyIP(5000, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
 
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps();
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
 builder.Services.AddSingleton<Mode>();
 builder.Services.AddCodeFirstGrpc();
 builder.Services.AddControllers();
